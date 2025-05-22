4caf7213d990fe618ce851630b52f1476e36eff8
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2025-05-21 23:56:01 -0300

    feat: product

---
+++ src/main/java/store/product/ProductController.java
@@ -0,0 +1,29 @@
+package store.product;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "product", url = "http://product:8080")
+public interface ProductController {
+
+    @PostMapping("/product")
+    ResponseEntity<ProductOut> create(
+        @RequestBody ProductIn productIn
+    );
+
+    @GetMapping("/product")
+    ResponseEntity<List<ProductOut>> findAll();
+
+    @GetMapping("/product/{id}")
+    ResponseEntity<ProductOut> findById(
+        @PathVariable("id") String id
+    );
+
+    @DeleteMapping("/product/{id}")
+    ResponseEntity<Void> delete(
+        @PathVariable("id") String id
+    );
+}
